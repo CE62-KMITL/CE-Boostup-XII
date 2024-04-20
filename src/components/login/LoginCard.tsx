435741d9f03ec46ddee292b24e992517ca39a96d
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2024-04-20 12:44:31 +0700

    Mook Crisis : Update LoginCard component styling and index.css

--- src/components/login/LoginCard.tsx
+++ src/components/login/LoginCard.tsx
@@ -3,19 +3,20 @@ import LogoIcon from "./LogoIcon"
 function LoginCard() {
     return (
         <div className="w-[482px] h-[704px] relative AbsoluteCentre">
-            <div className="absolute translate-x-[-50%] top-0 left-1/2 z-10">
+            <div className="absolute translate-x-[-50%] top-0 left-[50%] z-10">
                 <LogoIcon />
             </div>
             <div 
-            className="w-full h-[527px] bg-slate-600
-            absolute translate-x-[-50%] bottom-0 left-1/2 z-0">
-                <div className="bg-stone01 w-[128px] h-full 
+            className="w-full h-[527px] absolute 
+            translate-x-[-50%] bottom-0 left-[50%] z-0">
+                <div className="bg-stone01 w-[124px] h-full 
                 absolute left-0 rounded-tl-[20px] rounded-tr-[10px] 
                 rounded-bl-[20px] rounded-br-[0px]"></div>
-                <div className="bg-stone01 w-[128px] h-full 
+                <div className="bg-stone01 w-[124px] h-full 
                 absolute right-0 rounded-tl-[10px] rounded-tr-[20px] 
                 rounded-bl-[0px] rounded-br-[20px]"></div>
-                <div className="bg-stone01 w-full h-[464px] rounded-[20px] absolute left-0 bottom-0"></div>
+                <div className="bg-stone01 w-full h-[460px] rounded-[20px] 
+                absolute left-0 bottom-0"></div>
                 <div className="center-above-holder">
                     <div className="curved-corner-bottomleft"></div>
                     <div className="curved-corner-bottomright"></div>
