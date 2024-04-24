136ae8a6081af00571daf62987230ae162cd434d
gooseknight <coppertrin@gmail.com>
2024-04-24 19:45:11 +0700

    Test Structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import NavBar from './components/utils/NavBar'
 import SolveProblemPage from './pages/SolveProblemPage'
 import LoginPage from './pages/LoginPage'
+import LearnPage from './pages/LearnPage'
 
 function App() {
   return (
     <>
-      <LoginPage/>
+      {/* <LoginPage/> */}
       {/* <SolveProblemPage/> */}
+      <LearnPage/>
     </>
   )
 }
---
+++ src/pages/LearnPage.tsx
@@ -0,0 +1,7 @@
+function LearnPage() {
+    return(
+        <div className="bg-emerald-500 w-52 h-52">Hello World</div>
+    );
+}
+
+export default LearnPage
