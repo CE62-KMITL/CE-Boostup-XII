4c2d0de978aaadbc0ef67e2cf77b29b92e7c5e25
Proxiermax <89377437+Proxiermax@users.noreply.github.com>
2024-04-25 17:12:48 +0700

    Update component styling in LoginBackground.tsx and LoginForm.tsx

--- src/components/login/LoginCard/LogoIcon.tsx
+++ src/components/login/LoginCard/LogoIcon.tsx
@@ -2,7 +2,7 @@ function LogoIcon() {
     return (
         <div 
         className="bg-stone01 flex justify-center items-center 
-        absolute -translate-x-1/2 left-1/2 top-0 w-[220px] h-[230px] rounded-full">
+        absolute -translate-x-1/2 left-1/2 top-0 w-[230px] h-[230px] rounded-full">
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmYZNrSIj5yMGJbBNG7WaU1R4CviC8hSbU9Q&s" 
             className="w-[200px] h-[200px] rounded-full" alt="" />
         </div>
