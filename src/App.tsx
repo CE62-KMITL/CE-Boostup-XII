a06d869f4cd8c8df36331a9239e2ed7404cd142c
Tamtikorn <gan_zaza_12@hotmail.com>
2024-05-10 17:18:41 +0700

    feat: Add registration page for user sign up

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   )
