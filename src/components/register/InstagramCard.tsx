a560301e39c3f5ae938f332292d7d97e2510c6b1
Tamtikorn <gan_zaza_12@hotmail.com>
2024-05-20 19:20:26 +0700

    feat: Update InstagramCard to use a different Instagram post URL
    This commit updates the InstagramCard component to use a different Instagram post URL. The previous URL was specific to a particular post, but the new URL is more general and can be used to display any Instagram post. This change improves the flexibility and reusability of the component.

--- src/components/register/InstagramCard.tsx
+++ src/components/register/InstagramCard.tsx
@@ -3,9 +3,7 @@ import { InstagramEmbed } from 'react-social-media-embed';
 function RegisterFormCard() {
     return (
         <div className="w-full h-full rounded-[30px] xl:p-[30px] 2xl:p-[36px] bg-stone01">
-            <div className="w-full h-full overflow-y-scroll hide-scrollbar">
-                <InstagramEmbed url="https://www.instagram.com/p/C6-WrQCrQ1R/?hl=th"/>
-            </div>
+            <InstagramEmbed url="https://www.instagram.com/p/C7LPZQzr2XC/?hl=th&img_index=1" />
         </div>
     )
 }
