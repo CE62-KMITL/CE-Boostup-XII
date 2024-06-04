e679477c684cadabeccaae25eae1b96ea76dafff
gooseknight <coppertrin@gmail.com>
2024-06-04 11:28:42 +0700

    revert learn page

--- src/components/learn/LessonCardComponents/LessonGroup.tsx
+++ src/components/learn/LessonCardComponents/LessonGroup.tsx
@@ -2,7 +2,7 @@ import LessonCard from "../LessonCard";
 
 function LessonGroup({ lessons }: { lessons: { title: string; description: string; img: string }[] }) {
     return (
-        <div className=" w-auto h-auto grid grid-cols-3 gap-x-[19px] gap-y-[36px]">
+        <div className=" w-auto h-auto grid grid-cols-5 gap-x-[19px] gap-y-[36px]">
             {lessons.map((lesson, index) => (
                 <LessonCard key={index} title={lesson.title} description={lesson.description} img={lesson.img} />
             ))}
