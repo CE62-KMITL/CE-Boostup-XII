55a66ce4467656306dde45a6168fa6e44fc48cca
Proxiermax <teethawat9438@gmail.com>
2024-06-09 12:18:08 +0700

    Important : Create checkmode-page refactor: Update InstagramCard and EditorFooter components

--- src/components/register/InstagramCard.tsx
+++ src/components/register/InstagramCard.tsx
@@ -2,7 +2,7 @@ import { InstagramEmbed } from 'react-social-media-embed';
 
 function RegisterFormCard() {
     return (
-        <div className="w-full h-full rounded-[30px] xl:p-[30px] 2xl:p-[36px] bg-stone01">
+        <div className="w-full h-full rounded-[30px] p-[30px] 2xl:p-[36px] bg-stone01">
             <InstagramEmbed url="https://www.instagram.com/p/C7LPZQzr2XC/?hl=th&img_index=1" />
         </div>
     )
